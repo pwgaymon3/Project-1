b362274c8827a88e7b12beb0d64a2fa7384a6c56
Preston Gaymon <p.w.gaymon3@gmail.com>
2025-11-13 18:26:04 -0700

    controls-2 Changes

--- Assignments/controls-2.js
+++ Assignments/controls-2.js
@@ -1,43 +1,43 @@
 function isLarger(num1, num2) {
-  if(num1 > num2) {
-    return num1 + " is larger."
-  } else if(num2 > num1) {
-    return num2 + " is larger."
+  if (num1 > num2) {
+    return num1 + " is larger.";
+  } else if (num2 > num1) {
+    return num2 + " is larger.";
   } else {
-    return "The numbers are equal."
+    return "The numbers are equal.";
   }
 }
 
-console.log(isLarger(7, 8))
-console.log(isLarger(20,6))
-console.log(isLarger(5,5))
+console.log(isLarger(7, 8));
+console.log(isLarger(20, 6));
+console.log(isLarger(5, 5));
 
 function isSafeAirspeed(speed) {
-  if(speed >= 184 && speed <= 583) {
-    return true
-  } else return false
+  if (speed >= 184 && speed <= 583) {
+    return true;
+  } else return false;
 }
 
-console.log(isSafeAirspeed(186))
-console.log(isSafeAirspeed(70))
-console.log(isSafeAirspeed(588))
+console.log(isSafeAirspeed(186));
+console.log(isSafeAirspeed(70));
+console.log(isSafeAirspeed(588));
 
 function confirmAge(age) {
-  if(age <= 0){
-    return "Invalid age"
-  } else if(age < 13) {
-    return "You're too young to participate."
-  } else if(age >= 13 && age <=17) {
-    return "You are allowed to participate."
-  } else return "You're too old to participate."
+  if (age <= 0) {
+    return "Invalid age";
+  } else if (age < 13) {
+    return "You're too young to participate.";
+  } else if (age >= 13 && age <= 17) {
+    return "You are allowed to participate.";
+  } else return "You're too old to participate.";
 }
-console.log(confirmAge(-1))
-console.log(confirmAge(21))
-console.log(confirmAge(13))
-console.log(confirmAge(12))
+console.log(confirmAge(-1));
+console.log(confirmAge(21));
+console.log(confirmAge(13));
+console.log(confirmAge(12));
 console.log(typeof []);
 
 function gitTest() {
-  console.log("This is just to test git")
+  console.log("This is just to test git");
 }
-gitTest()
+gitTest();
